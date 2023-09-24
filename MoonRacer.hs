5a4ad64dc862f7898188d3fb8157ffb4e52b9b94
Tristan Cacqueray <tdecacqu@redhat.com>
2023-09-24 22:10:04 +0000

    Add velocity vector

--- MoonRacer.hs
+++ MoonRacer.hs
@@ -6,7 +6,13 @@ import Data.Bool
 import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Interact
 
+{-
 import Debug.Trace
+debugWorld :: Float -> World -> a -> a
+debugWorld ts (_, pos, vel, acc, _) = traceShow ("ts", ts, "pos", pos, "vel", vel, "acc", acc)
+debugStep :: Float -> State -> State
+debugStep ts s@(_, w) = debugWorld ts w (step ts s)
+-}
 
 type World = ((Float, Float), (Float, Float), (Float, Float), (Float, Float), [(Float, Float)])
 initialWorld :: World
@@ -25,45 +31,65 @@ initialStars =
 
 type State = (String, World)
 
+translatePoint :: (Float, Float) -> Picture -> Picture
+translatePoint (x, y) = translate (x * 10) (y * 10)
+scaleUniform :: Float -> Picture -> Picture
+scaleUniform factor = scale factor factor
+
 drawCircle :: (Float, Float) -> Picture
-drawCircle (x, y) = translate (x * 10) (y * 10) (Circle 3)
+drawCircle pos = translatePoint pos (Circle 3)
+
+drawVelocy :: (Float, Float) -> (Float, Float) -> Picture
+drawVelocy pos v = translatePoint pos (scaleUniform 10 $ line [origin, v])
 
 draw :: State -> Picture
-draw (status, (_, p, _, _, g : _)) = translate 0 (-297) (scale 0.1 0.1 (Text status) <> drawCircle p <> drawCircle g)
--- draw (status, w@(_, p, v, _, g : _)) = translate 0 (-297) (scale 0.1 0.1 (Text (show (status, p, v))) <> drawCircle p <> drawCircle g)
+draw (status, (_, p, v, _, g : _)) =
+    translatePoint
+        (0, -29.7)
+        ( scaleUniform 0.1 (Text status)
+            <> drawCircle p
+            <> drawVelocy p v
+            <> drawCircle g
+        )
 
 eventKey :: Event -> (SpecialKey, Float)
-eventKey (EventKey (SpecialKey key) d _ _) = (key, bool 0 1 $ d == Down)
+eventKey (EventKey (SpecialKey key) d _ _) = (key, bool 1 0 $ d == Up)
 eventKey _ = (KeyF1, 0)
 
 event :: Event -> State -> State
 event ev (status, (score, pos, vel, (x, y), stars)) =
-    let (k, d) = eventKey ev
+    let (k, isPressed) = eventKey ev
         ax
-            | k == KeyRight = d
-            | k == KeyLeft = -1 * d
+            | k == KeyRight = isPressed
+            | k == KeyLeft = negate isPressed
             | otherwise = x
         ay
-            | k == KeyUp = d
-            | k == KeyDown = -1 * d
+            | k == KeyUp = isPressed
+            | k == KeyDown = negate isPressed
             | otherwise = y
      in (status, (score, pos, vel, (ax, ay), stars))
 
+damp = (*) 0.8
+gravity = 0.3
+
 step :: Float -> State -> State
-step ts (status, ((score, time), (px, py), (vx, vy), a@(ax, ay), (sx, sy) : r)) =
+step ts (status, ((score, time), (px, py), (vx, vy), accel@(ax, ay), stars@((sx, sy) : r))) =
     let hit m n = abs (m - n) < 1
-        (newScore, rest) = if hit px sx && hit py sy then (score + 1, r) else (score, (sx, sy) : r)
+        (newScore, rest)
+            | hit px sx && hit py sy = (score + 1, r)
+            | otherwise = (score, stars)
         newPx = px + (vx + ax) * ts * 10
         newPy = max 0 (py + (vy + ay) * ts * 10)
-        newVx = (ax + vx) * 0.8
-        newVy = (ay + vy) * 0.8 + (-0.5)
+        newVx = damp (ax + vx)
+        newVy = damp (ay + vy - gravity)
+        nextWorld = ((newScore, time + ts), (newPx, newPy), (newVx, newVy), accel, rest)
         (newStatus, newWorld)
             | score > 9 = ("Best: " <> show time, initialWorld)
-            | otherwise = (status, ((newScore, time + ts), (newPx, newPy), (newVx, newVy), a, rest))
+            | otherwise = (status, nextWorld)
      in (newStatus, newWorld)
 
 win :: Display
-win = InWindow "MoonRacer" (800, 600) (20, 20)
+win = InWindow "MoonRacer" (800, 600) (5, 5)
 
 main :: IO ()
-main = play win white 30 ("", initialWorld) draw event step
+main = play win white 30 ("", initialWorld) draw event {-debug-} step
--- MoonRacer.hs.min.hs
+++ MoonRacer.hs.min.hs
@@ -1,10 +1,19 @@
-import Imports;a=(b,b,b,b,[(h(l`i`70-35),h(5+l`i`50))|l<-[0,88..]]);b=(0,0)
-c(l,m)=k(l*10)(m*10)(Circle 3);main=play(w(20,20))white 30("",a)d f g;i=mod
-d(l,(_,m,_,_,n:_))=k 0(-297)(scale 0.1 0.1(Text l)<>c m<>c n);y=(800,600)
-e(EventKey(SpecialKey l)m _ _)=(l,bool 0 1$m==Down);e _=(KeyF1,0);z= -1;
-w=(InWindow"MR"y);k=translate;j=True;h=fromIntegral;f l(m,(n,o,p,(q,r),s))=let
- (t,u)=e l;v|t==KeyRight=u|t==KeyLeft=z*u|j=q;w|t==KeyUp=u|t==KeyDown=z*u|j=r
- in (m,(n,o,p,(v,w),s));g l(m,((n,o),(p,q),(r,s),t@(u,v),(w,x):y))=let
- z ι κ=abs(ι-κ)<1;(α,β)=if z p w&&z q x then(n+1,y)else(n,(w,x):y);
- γ=p+(r+u)*l*10;δ=max 0(q+(s+v)*l*10);ε=(u+r)*0.8;ζ=(v+s)*0.8+(-0.5);
- (η,θ)|n>9=("Best: "<>show o,a)|j=(m,((α,o+l),(γ,δ),(ε,ζ),t,β)) in (η,θ);
+import Imports;a=(b,b,b,b,[(m(p`mod`70-35),m(5+p`mod`50))|p<-[0,88..]]);b=(0,0)
+c(p,q)=translate(p*10)(q*10);d p=scale p p;e p=c p(Circle 3);y=InWindow"MoonRac"
+f p q=c p(d 10$line[b,q]);h(EventKey(SpecialKey p)q _ _)=(p,bool 1 0$q==Up);
+h _=(KeyF1,0);g(p,(_,q,r,_,s:_))=c(0,-29.7)(d 0.1(Text p)<>e q<>f q r<>e s);
+z=y(800,600)(5,5);l=10;m=fromIntegral;n=negate;o=True;i p(q,(r,s,t,(u,v),w))=let
+ (x,y)=h p;z|x==KeyRight=y|x==KeyLeft=n y|o=u;α|x==KeyUp=y|x==KeyDown=n y|o=v in
+ (q,(r,s,t,(z,α),w));j=(*)0.8;k p(q,((r,s),(t,u),(v,w),x@(y,z),α@((β,γ):δ)))=let
+ ε ο π=abs(ο-π)<1;(ζ,η)|ε t β&&ε u γ=(r+1,δ)|o=(r,α);θ=t+(v+y)*p*10;κ=j(y+v);
+ ι=max 0(u+(w+z)*p*10);λ=j(z+w-0.3);μ=((ζ,s+p),(θ,ι),(κ,λ),x,η);
+ (ν,ξ)|r>9=("Best: "<>show s,a)|o=(q,μ)in(ν,ξ);main=play z white 30("",a)g i k;
+-- ^10 ------------------------------------------------------------------ 80> --
+{- hackage-10-80/moon-racer (tristanC)
+
+- Play: stack runghc MoonRacer.hs.min.hs --resolver lts-20.13 --package gloss
+- Keys: arrow to fly the ship
+
+Copyright 2023, Tristan de Cacqueray
+SPDX-License-Identifier: CC-BY-4.0
+-}
